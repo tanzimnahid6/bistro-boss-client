657e6474d9a309c0619a838cf54b63335ef5521a
tanzim-nahid <tanzimnahid6@gmail.com>
2023-05-29 17:35:26 +0600

    part3 done

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -4,14 +4,14 @@ import { useQuery } from "@tanstack/react-query"
 
 const useCart = () => {
   const { user } = useContext(AuthContext)
-
+//=================================================TAN STACK QUERY===========================================
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
       const res = await fetch(
         `http://localhost:5000/carts?email=${user?.email}`
       )
-    //   console.log(typeof refetch());
+
       return res.json()
     },
   })
